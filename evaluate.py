52e796ce14f66a9497122a970dec45307ac17c23
Stephen <stephen_wu_3@sfu.ca>
2019-11-15 00:27:47 -0800

    make it work

--- evaluate.py
+++ evaluate.py
@@ -11,8 +11,8 @@ import time
 import json
 import subprocess
 import numpy
-from moviepy.video.io.VideoFileClip import VideoFileClip
-import moviepy.video.io.ffmpeg_writer as ffmpeg_writer
+#from moviepy.video.io.VideoFileClip import VideoFileClip
+#import moviepy.video.io.ffmpeg_writer as ffmpeg_writer
 
 BATCH_SIZE = 4
 DEVICE = '/gpu:0'
